b60c9ee93c2f011693dae24e50da7fbc40e12749
Shrey Ardeshana <ardeshanashrey17@gmail.com>
2025-03-08 16:57:13 +0530

    Added Drown class implementing SwimBehavior

---
+++ Drown.java
@@ -0,0 +1,6 @@
+// Class for ducks that drown (not a real duck behavior, just for the example)
+public class Drown implements SwimBehavior {
+    public void swim() {
+        System.out.println("This duck is drowning!");
+    }
+}
